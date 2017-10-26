f5f9cedd5efc75e15cec1fd5df670bb65389f5c9
juanbc <jbc.develop@gmail.com>
2017-10-26 14:54:28 -0300

    bump

--- libs/container.py
+++ libs/container.py
@@ -8,14 +8,9 @@ import pandas as pd
 
 class Container(dict):
     
-    def __init__(self, path):
-        files = []
-        for fpath in glob.glob(os.path.join(path, "*.npy")):
-            fname = os.path.basename(fpath).split("_", 1)[0]
-            print "Loading '{}'...".format(fpath)
-            self[fname] = pd.DataFrame(np.load(fpath))
-            files.append(fname)
-        self._repr = "{" + ", ".join(files) + "}"
+    def __init__(self, data):
+        super(Container, self).__init__(data)
+        self._repr = "{" + ", ".join(data.keys()) + "}"
     
     def __dir__(self):
         return list(self.keys())
@@ -26,3 +21,13 @@ class Container(dict):
     def __getattr__(self, an):
         return self[an]
         
+        
+def read(path):
+    data = {}
+    for fpath in glob.glob(os.path.join(path, "*.npy")):
+        fname = os.path.basename(fpath).split("_", 1)[0]
+        print "Loading '{}'...".format(fpath)
+        data[fname] = pd.DataFrame(np.load(fpath))
+    return Container(data)
+        
+    
